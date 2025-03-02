f29d6df002e3f72a0c4230b5019b18ed708dc024
Ebrahim112002 <md1847094@gmail.com>
2025-03-02 21:22:24 +0600

    added final code

--- js/script.js
+++ js/script.js
@@ -29,10 +29,30 @@ function liveDate() {
 
 window.onload = liveDate;
 
-// button work
 
+// add a new log entry to the activity log
+function addActivityLog(taskTitle) {
+    const activityLog = document.getElementById("activityLog");
+    const now = new Date();
+    const timeString = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second:'2-digit', hour12: true });
+
+    // Create a new log 
+    const logEntry = document.createElement("div");
+    logEntry.className = "p-4 border-b border-dashed border-[#3752fd]/20 last:border-b-0";
+    logEntry.innerHTML = `
+    <div class = "py-6 w-full p-2.5 bg-[#f4f7ff] rounded-2xl justify-center items-start gap-2.5 inline-flex">
+        <p class="text-[#00303c]  text-base font-medium tracking-wide">You have completed the task : <span class="font-bold">${taskTitle} at ${timeString}</span> </p>
+        
+        </div>
+    `;
+
+    activityLog.appendChild(logEntry);
+    activityLog.classList.remove("hidden");
+}
+
+// Button work
 const checkBox = document.getElementById("checkbox");
-const task = document.getElementById("taskAssign")
+const task = document.getElementById("taskAssign");
 const btns = document.querySelectorAll(".cmplete-btn");
 
 btns.forEach(function(btn) {
@@ -48,6 +68,10 @@ btns.forEach(function(btn) {
                 checkBox.innerText = ChecKbox + 1;
                 event.target.disabled = true;
                 event.target.style.backgroundColor = "gray";
+
+                const taskTitle = event.target.closest('.task-card').querySelector('.task-item').innerText;
+
+                addActivityLog(taskTitle);
             }
             let allDisabled = true;
             for (let btn of btns) {
@@ -64,4 +88,9 @@ btns.forEach(function(btn) {
     });
 });
 
-
+// Clear History Button
+document.querySelector("#clear-btn").addEventListener('click', function() {
+    const activityLog = document.getElementById("activityLog");
+    activityLog.innerHTML = ""; 
+    activityLog.classList.add("hidden");
+});
